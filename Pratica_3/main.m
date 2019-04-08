03d2cc06f722ae45b2ff3e0fce11d93f67ea2779
LucasHenriqueP <lucashenrique.pereira@hotmail.com>
2019-04-08 14:39:56 -0300

    Pratica 3 feita em sala de aula

---
+++ Pratica_3/main.m
@@ -0,0 +1,40 @@
+pkg load image;
+
+m = imread("imagem.jpg");
+
+[counts, x] = imhist(m);
+
+[M, N] = size(m);
+
+mat = im2uint8(zeros(M,N));
+
+arr = zeros(size(counts));
+resultados = zeros(size(counts));
+
+
+arr = counts./(M*N);
+
+resultados(1) = 255 * arr(1);
+
+for i = 2:256
+
+resultados(i) = (255*arr(i)) + resultados(i-1);
+
+endfor
+
+for i = 1:M
+  for j = 1:N
+    mat(i,j) = resultados(m(i,j));
+   endfor
+endfor
+
+figure(1),plot(resultados);
+
+figure(2),imshow(mat);
+
+imwrite(mat, "img_corrected.jpg", "quality", 100);
+
+
+    
+
+
