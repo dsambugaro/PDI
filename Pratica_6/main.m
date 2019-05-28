efd1ad0ba83d49d860efacb622c73d802701a6d3
LucasHenriqueP <lucashenrique.pereira@hotmail.com>
2019-05-28 18:01:58 -0300

    Feita a pratica 6

--- Pratica_6/main.m
+++ Pratica_6/main.m
@@ -9,11 +9,13 @@ imsize = size(original);
 imsize
 
 transformada =  fft2(original, imsize(1)*2, imsize(2)*2);
-
+% A dimensao da transformada e M*2, por N*2 (O dobro da imagem original)
+% Neste caso 512*512;
+ 
 transformada = fftshift(transformada);
 
 
-imshow(uint8(abs(transformada)));
+figure(1),imshow(uint8(abs(transformada)));
 
 filtro = zeros(size(transformada));
 
@@ -27,5 +29,16 @@ for u=1:P
   D = sqrt((u - P/2)^2 + (v - Q/2)^2);
   filtro(u,v) = e^(-(D^2)/(2*(D0^2)));
   endfor
-
 endfor
+
+figure(2), imshow(filtro)
+
+transformada = transformada.* filtro;
+transformada = ifftshift(transformada);
+transformada = ifft2(transformada);
+transformada = real(transformada);
+resultado = transformada([1:imsize(1)], [1:imsize(2)]);
+
+resultado = im2uint8(resultado);
+
+figure(3),imshow(resultado)
