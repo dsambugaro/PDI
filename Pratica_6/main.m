22f8be4a0a0bcea085c91abfd0b598b77d4671e6
dsambugaro <daniloc@alunos.utfpr.edu.br>
2019-06-02 13:06:40 -0300

    update

--- Pratica_6/main.m
+++ Pratica_6/main.m
@@ -1,26 +1,23 @@
 pkg load image
 
 original = imread("pratica6.png");
-
 original = im2double(original);
-
 imsize = size(original);
 
-imsize
 
 transformada =  fft2(original, imsize(1)*2, imsize(2)*2);
 % A dimensao da transformada e M*2, por N*2 (O dobro da imagem original)
 % Neste caso 512*512;
- 
+
 transformada = fftshift(transformada);
+espectro = transformada;
 
-
-figure(1),imshow(uint8(abs(transformada)));
+figure(1),imshow(uint8(abs(espectro)));
 
 filtro = zeros(size(transformada));
 
-P = size(transformada)(1)
-Q = size(transformada)(2)
+P = size(transformada)(1);
+Q = size(transformada)(2);
 
 D0 = 20;
 
@@ -31,14 +28,16 @@ for u=1:P
   endfor
 endfor
 
-figure(2), imshow(filtro)
+figure(2), imshow(filtro);
 
 transformada = transformada.* filtro;
-transformada = ifftshift(transformada);
-transformada = ifft2(transformada);
-transformada = real(transformada);
-resultado = transformada([1:imsize(1)], [1:imsize(2)]);
+imwithfiltro = ifftshift(transformada);
+imwithfiltro = ifft2(imwithfiltro);
+imwithfiltro = real(imwithfiltro);
+resultado = imwithfiltro([1:imsize(1)], [1:imsize(2)]);
 
 resultado = im2uint8(resultado);
 
-figure(3),imshow(resultado)
+figure(3),imshow(resultado);
+
+imwrite(resultado, 'resultado_transformada_inversa_rapida.png', "quality", 100);
